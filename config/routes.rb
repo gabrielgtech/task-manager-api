fe1207506614015e338c4d1471d5d60a9ff64691
gabrielgtech <gabriel.sata@gmail.com>
2017-06-30 17:23:07 -0300

    adding api version contraint to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'api_version_constraint'
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :api, defaults:{format: :json}, constraints:{subdomain: 'api'},  path: "/" do
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
 
+    end
   end
 end
