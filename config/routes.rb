1a109103368d31fa45024cb076c26559e80d8f41
gabrielgtech <gabriel.sata@gmail.com>
2017-06-30 19:01:09 -0300

    adding devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'api_version_constraint'
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :api, defaults:{format: :json}, constraints:{subdomain: 'api'},  path: "/" do
