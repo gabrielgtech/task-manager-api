bd2f796dad6d6e89088ca0be3c424d1e0cf07cd3
gabrielgtech <gabriel.sata@gmail.com>
2017-07-08 14:46:10 -0300

    chaging double quotes to simple quotes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 # devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  namespace :api, defaults:{format: :json}, constraints:{subdomain: 'api'},  path: "/" do
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults:{format: :json}, constraints:{subdomain: 'api'},  path: '/' do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show]
     end
   end
